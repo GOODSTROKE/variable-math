284ee358ca298467cd0e6caf0cd154eca7863413
Raulen A Conway <Harmongroupmedia@gmail.com>
2022-10-10 15:21:17 -0400

    Finished Variable math assignment

---
+++ fute-age.js
@@ -0,0 +1,5 @@
+let age = 26;
+//console.log("In 5 years my age will be ${age+5");
+
+let newAge = age + 5
+consolel.log("In 5 years i will be + newAge")
---
+++ greeter.js
@@ -0,0 +1,5 @@
+let myName1 = "Alister";
+
+console.log(`Hello my name is ${myName1} nice to meet you `);
+
+console.log("Hello my name is " + myName1 + " and it is nive to meet you");
